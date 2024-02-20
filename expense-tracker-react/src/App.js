4e72e14af0b5364ed609163c5a4098da2c10cd5e
Sanjana <sanjana.js02@gmail.com>
2024-02-20 15:52:07 +0100

    completed

---
+++ expense-tracker-react/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TranscationList } from "./components/TranscationList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TranscationList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
